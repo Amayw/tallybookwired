16c73f5708dd0383d5b67ea4d546af0725468b6b
wywang <727936199@qq.com>
2021-01-25 22:31:23 +0800

    收集NumberPad组件的数据到Money组件

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -8,22 +8,32 @@ type Consumption={
   selectedId:number,
   category:'-'|'+',
   note:string,
-  amount:string
+  amount:string,
+  date:string
 }
 export default function Money(){
   const [consumption,setConsumption]=useState<Consumption>({
     selectedId:2,
     category:'-',
     note:'',
-    amount:'0'
+    amount:'0',
+    date:(new Date()).toString().split(' ').slice(1,4).join(',')
   });
 
   return (
     <Layout>
-      {consumption.selectedId}{consumption.category}
-      <Category category={consumption.category} onChange={category=>setConsumption({...consumption,category})}/>
-      <Tags selectedId={consumption.selectedId} onChange={selectedId=>setConsumption({...consumption,selectedId})}/>
-      <NumberPad/>
+      {consumption.selectedId}{consumption.category}{consumption.date}{consumption.note}{consumption.amount}
+      <Category category={consumption.category}
+                onChange={category=>setConsumption({...consumption,category})}/>
+      <Tags selectedId={consumption.selectedId}
+            onChange={selectedId=>setConsumption({...consumption,selectedId})}/>
+      <NumberPad note={consumption.note}
+                 onChange1={note=>setConsumption({...consumption,note})}
+                 amount={consumption.amount}
+                 onChange2={amount=>setConsumption({...consumption,amount})}
+                 date={consumption.date}
+                 onChange3={date=>setConsumption({...consumption,date})}
+                 onOk={()=>{}}/>
     </Layout>
   )
 }
