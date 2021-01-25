eba3cab0f57ff3ad8831751719d645fd64226ad1
wywang <727936199@qq.com>
2021-01-25 10:50:07 +0800

    完成Category组件样式和功能

--- src/components/money/Category.tsx
+++ src/components/money/Category.tsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import {WiredButton} from 'react-wired-elements';
 
 const CategoryWrapper=styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  >wired-button{
+  >ol>li{
     margin: 12px 20px;
+    display: inline-block;
     &.active{
-      background: #f7d26a;
+    background: #f7d26a;
     }
   }
 `
 
 
 export default function Category(){
+  const categoryMap={'-':'支出','+':'收入'};
+  type categoryType=keyof typeof categoryMap;
+  const [categoryList]=useState<categoryType[]>(['-','+']);
+  const [category,setCategory]=useState<categoryType>('-');
   return (
     <CategoryWrapper>
-
+      <ol>
+        {
+          categoryList.map(c=>(
+            <li key={c} className={category===c?'active':''} onClick={()=>setCategory(c)}>
+              <WiredButton>
+                {categoryMap[c]}
+              </WiredButton>
+            </li>
+          ))
+        }
+      </ol>
     </CategoryWrapper>
   )
 }
