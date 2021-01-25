6a2860b26311d098359246d22b5d13f2c7e10e1a
wywang <727936199@qq.com>
2021-01-25 21:24:08 +0800

    完成NumberPad页面的输入金额功能和输入备注功能

--- src/components/money/NumberPad.tsx
+++ src/components/money/NumberPad.tsx
@@ -11,6 +11,9 @@ const NumberPadWrapper=styled.div`
   align-items: center;
   >.inputs{
     margin: 6px 10px;
+    display: flex;
+    flex-direction: row;
+    max-width: 500px;
     >.left{
       width: 60vw;
       padding: 4px;
@@ -36,32 +39,64 @@ const NumberPadWrapper=styled.div`
   }
 `
 const NumberPad:React.FC=()=>{
+  const [note,setNote]=useState('');
+  const [output,setOutput]=useState('0');
   const [toggleOpen,setToggleOpen]=useState<boolean>(false);
   const [date,setDate]=useState<string>('');
+
   const onChangeDate=(e:CustomEvent)=>{
     setDate(e.detail.selected);
   }
+
+  const handleButton=(e: React.MouseEvent)=>{
+    const text=(((e.target as HTMLButtonElement).textContent) as string).trim();
+    if('0123456789'.split('').indexOf(text)>=0){
+      if (output === '0') {
+        setOutput(text);
+      } else {
+        setOutput(output + text);
+      }
+    }else if(text==='删除'){
+      if(output.length>=1){
+        setOutput(output.slice(0,-1));
+      }
+
+      if(output.length===1||output===''){
+        setOutput('0');
+      }
+    }else if(text==='清空'){
+      setOutput('0');
+    }else if(text==='.'){
+      if(output.indexOf('.')>=0){
+        return;
+      }
+      setOutput(output + text);
+    }else if(text==='完成'){
+      console.log('ok');
+    }
+  }
+
   return (
     <NumberPadWrapper>
        <div className='inputs'>
-         <WiredInput className='left' placeholder='请输入备注'/>
-         <WiredInput className='right' placeholder='0元'/>
+         <WiredInput className='left' placeholder='请输入备注' value={note} onBlur={e=>setNote(e.target.value)}/>
+         <WiredInput className='right' value={output}/>
        </div>
-       <div className='buttons'>
-         <WiredButton elevation={2}>1</WiredButton>
-         <WiredButton>2</WiredButton>
-         <WiredButton>3</WiredButton>
-         <WiredButton>4</WiredButton>
+       <div className='buttons' onClick={handleButton}>
+         <WiredButton>&nbsp;1&nbsp;</WiredButton>
+         <WiredButton>&nbsp;2&nbsp;</WiredButton>
+         <WiredButton>&nbsp;3&nbsp;</WiredButton>
+         <WiredButton>&nbsp;4&nbsp;</WiredButton>
          <WiredButton elevation={2} onClick={()=>setToggleOpen(!toggleOpen)}>日历
          </WiredButton>
-         <WiredButton>5</WiredButton>
-         <WiredButton>6</WiredButton>
-         <WiredButton>7</WiredButton>
-         <WiredButton>8</WiredButton>
+         <WiredButton>&nbsp;5&nbsp;</WiredButton>
+         <WiredButton>&nbsp;6&nbsp;</WiredButton>
+         <WiredButton>&nbsp;7&nbsp;</WiredButton>
+         <WiredButton>&nbsp;8&nbsp;</WiredButton>
          <WiredButton>删除</WiredButton>
-         <WiredButton>9</WiredButton>
-         <WiredButton>0</WiredButton>
-         <WiredButton>.</WiredButton>
+         <WiredButton>&nbsp;9&nbsp;</WiredButton>
+         <WiredButton>&nbsp;0&nbsp;</WiredButton>
+         <WiredButton>&nbsp;.&nbsp;</WiredButton>
          <WiredButton>清空</WiredButton>
          <WiredButton>完成</WiredButton>
        </div>
@@ -69,7 +104,7 @@ const NumberPad:React.FC=()=>{
         <div style={{textAlign: 'center'}}>
           <div>
             <WiredCalendar bgColor="white" dimmedColor="gray" elevation={1}
-              lineColor="black" locale="en" onSelect={e=>onChangeDate(e)} selectedColor="red" selectedDate={date}/>
+              lineColor="black" locale="en" onSelect={onChangeDate} selectedColor="red" selectedDate={date}/>
           </div>
           <br />
           <div>
