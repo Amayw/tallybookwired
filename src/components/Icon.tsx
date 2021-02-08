366af6a6ebfa6cb5385998babc531702b605ed77
wywang <727936199@qq.com>
2021-02-08 11:40:02 +0800

    重新封装Icon，让其支持所有svg的属性

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,17 +1,20 @@
 import React from "react"
 import styled from 'styled-components';
+import cs from 'classnames';
 
 const SvgWrapper=styled.svg`
     width: 26px;
     height: 26px;
 `
-//这里使用的typescript
+
 type Props={
   name:string,
-}
+}&React.SVGAttributes<SVGElement>
 const Icon:React.FC<Props>=(props)=>{
+  const {name,children,className,...rest}=props;
+
   return (
-      <SvgWrapper className='icon'>
+      <SvgWrapper className={cs('icon',className)} {...rest}>
         <use xlinkHref={'#'+props.name}/>
       </SvgWrapper>
   )
